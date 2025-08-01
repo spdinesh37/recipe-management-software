8ff14711a2d5cb3b8ac39f73679a18df90523011
dinesh <spdinesh3737@gmail.com>
2025-08-01 15:45:12 -0500

    initial commit

---
+++ src/main/java/com/recepies/recepies/MasterRecepiesApplication.java
@@ -0,0 +1,13 @@
+package com.recepies.recepies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasterRecepiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MasterRecepiesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/recepies/recepies/MasterRecepiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.recepies.recepies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MasterRecepiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
